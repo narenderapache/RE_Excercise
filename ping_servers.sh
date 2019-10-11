d6176a4617b3decd4b75813b9b212d49b790397a
Narender A <narenderapache@gmail.com>
2019-10-11 23:06:20 +0100

    shell script for ping

---
+++ ping_servers.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Assuming that only three servers are available at the moment
+# If we have more servers, we can put them in a file and read the server name from the file
+for server in google.com google.co.uk google.de
+do
+  echo "================ping started for $server==================="
+#  Ping the server for 10 seconds
+  ping -t 10  $server
+#  return code of the last executed command
+  if [ "$?" = "0" ]; then
+      echo "ping is successful for $server"
+      else
+        echo "server $server is unreachable"
+  fi
+  echo "================ping ended for $server====================="
+done
+
